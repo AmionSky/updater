2bd70c7673b8ae43ca4cf6e8cd1db4587a3e8b02
Csányi István <csanyistvan02@gmail.com>
2020-05-05 23:46:14 +0200

    Hide console by default on release builds Show console output when run from console

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 mod config;
 mod launcher;
 mod locker;
@@ -14,6 +16,9 @@ use std::error::Error;
 use std::path::PathBuf;
 
 fn main() {
+    #[cfg(target_os = "windows")]
+    attach_console();
+
     setup_logger();
     let cfg = load_config();
     start(&cfg);
@@ -116,20 +121,8 @@ fn logger_config() -> simplelog::Config {
     config.build()
 }
 
-/*
-fn self_rename() {
-    println!("Hello, world!");
-
-    println!("{}", std::env::current_dir().unwrap().display());
-    println!("{}", std::env::current_exe().unwrap().display());
-
-    let mut to = std::env::current_exe().unwrap();
-    to.set_extension("tmp");
-    if std::fs::remove_file(&to).is_ok() {
-        println!("Removed old tmp file");
-    }
-    std::fs::rename(std::env::current_exe().unwrap(), &to).unwrap();
-
-    println!("{}", std::env::current_exe().unwrap().display());
+#[cfg(target_os = "windows")]
+fn attach_console() {
+    use winapi::um::wincon::{AttachConsole, ATTACH_PARENT_PROCESS};
+    let _ = unsafe { AttachConsole(ATTACH_PARENT_PROCESS) };
 }
-*/
